6b198e1688e14df30ecd5e2bb8da56046200de8d
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2025-12-19 15:06:08 +0545

    New dash

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -3,48 +3,40 @@ import 'package:cine_stream/screens/bottom_navigation_screens/home_screen.dart';
 import 'package:cine_stream/screens/bottom_navigation_screens/profile_screen.dart';
 import 'package:cine_stream/screens/bottom_navigation_screens/search_screen.dart';
 import 'package:flutter/material.dart';
-
 class BottomNavigationScreen extends StatefulWidget {
   const BottomNavigationScreen({super.key});
 
   @override
-  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+  State<BottomNavigationScreen> createState() =>
+      _BottomNavigationScreenState();
 }
 
 class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
   int _selectedIndex = 0;
 
-  final List<Widget> _screens = [
-    const HomeScreen(),
-    const SearchScreen(),
-    const FavouriteScreen(),
-    const ProfileScreen(),
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    SearchScreen(),
+    FavouriteScreen(),
+    ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    setState(() => _selectedIndex = index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, 
       body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.black,
-        selectedItemColor: const Color.fromARGB(255, 32, 162, 223),
-        unselectedItemColor: Colors.grey,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            activeIcon: Icon(Icons.home_filled),
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
