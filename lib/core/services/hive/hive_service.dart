bdeb6c5c1ea01d4160e6ef55de23e89fd7ab53fc
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-02 23:35:19 +0545

    services

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,56 @@
+import 'package:cine_stream/core/constants/hive_table_constant.dart';
+import 'package:cine_stream/features/auth/data/models/auth_hive_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+final hiveServiceProvider = Provider<HiveService>((ref) {
+  return HiveService();
+});
+
+class HiveService {
+  // Initialize Hive
+  Future<void> init() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+    _registerAdapter();
+    await _openBoxes();
+  }
+
+  // register all types of adapters
+  void _registerAdapter() {
+    if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+
+  // open all boxes
+  Future<void> _openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTableName);
+  }
+
+  // close all boxes
+  Future<void> close() async {
+    await Hive.close();
+  }
+
+  // ===================== Auth operations =====================
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTableName);
+
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
+    await _authBox.put(model.userId, model);
+    return model;
+  }
+
+  Future<AuthHiveModel?> loginUser(String userName, String password) async {
+    final users = _authBox.values.where(
+      (user) => user.userName == userName && user.password == password,
+    );
+    if (users.isNotEmpty) {
+      return users.first;
+    }
+    return null;
+  }
+}
