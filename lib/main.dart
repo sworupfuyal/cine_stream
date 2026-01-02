d9df2618230137daf6412fb63f53472f5c0e2bc4
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-03 00:12:46 +0545

    finalizing

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'package:cine_stream/core/services/hive/hive_service.dart';
+import 'package:cine_stream/features/auth/presentation/pages/signin_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'themes/theme.dart';
 import 'screens/splash_screen.dart';
 import 'screens/onboarding_screen.dart';
-import 'screens/signup_screen.dart';
-import 'screens/signin_screen.dart';
+import 'features/auth/presentation/pages/signup_screen.dart';
 import 'screens/dashboard_screen.dart';
-import 'themes/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+    final hiveService = HiveService();
+  await hiveService.init(); 
+
+ 
+  runApp(
+    ProviderScope(
+
+       overrides: [
+        hiveServiceProvider.overrideWithValue(hiveService),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,8 +34,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        theme: AppTheme.darkTheme,
-
+      theme: AppTheme.darkTheme,
       initialRoute: "/splash",
       routes: {
         "/splash": (_) => const SplashScreen(),
