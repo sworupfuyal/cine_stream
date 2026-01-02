7364c16e5050f980fad5a9a63fb9bfe43b88ba5d
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-02 23:34:17 +0545

    new constants

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,9 @@
+class HiveTableConstant {
+  // Database Name
+  static const String dbName = "cinestream_db";
+
+  // Auth Table
+  static const int authTypeId = 0;
+  static const String authTableName = "auth_table";
+  
+}
