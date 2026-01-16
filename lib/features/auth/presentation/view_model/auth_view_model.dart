e7da982ce74411623dc80844c7016266e31dbfd0
sworupfuyal <149761522+sworupfuyal@users.noreply.github.com>
2026-01-16 19:59:45 +0545

    readjusted viwmodel features

--- lib/features/auth/presentation/view_model/auth_view_model.dart
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -1,3 +1,4 @@
+
 import 'package:cine_stream/features/auth/domain/usecases/login_usecases.dart';
 import 'package:cine_stream/features/auth/domain/usecases/register_usecases.dart';
 import 'package:cine_stream/features/auth/presentation/state/auth_state.dart';
@@ -19,33 +20,42 @@ class AuthViewModel extends Notifier<AuthState> {
   }
 
   // Register
-  Future<void> register({
-    required String userName,
-    required String password,
-  }) async {
-    state = state.copyWith(status: AuthStatus.loading);
-    final params = RegisterUsecasesParam(
-      userName: userName,
-      password: password,
-    );
-    final result = await _registerUsecase.call(params);
-    result.fold(
-      (failure) {
-        state = state.copyWith(
-          status: AuthStatus.error,
-          errorMessage: 'Registration Failed',
-        );
-      },
-      (success) {
-        state = state.copyWith(status: AuthStatus.registered);
-      },
-    );
-  }
+Future<void> register({
+  required String fullName,
+  required String email,
+  required String password,
+  required String confirmPassword,
+}) async {
+  state = state.copyWith(status: AuthStatus.loading);
+  final params = RegisterUsecasesParam(
+    fullName: fullName,
+    email: email,
+    password: password,
+    confirmPassword: confirmPassword,
+  );
+  final result = await _registerUsecase.call(params);
+  
+  print('Result type: ${result.runtimeType}'); // Add this
+  
+  result.fold(
+    (failure) {
+      print('FAILURE BRANCH: ${failure.toString()}'); // Add this
+      state = state.copyWith(
+        status: AuthStatus.error,
+        errorMessage: 'Registration Failed',
+      );
+    },
+    (success) {
+      print('SUCCESS BRANCH'); // Add this
+      state = state.copyWith(status: AuthStatus.registered);
+    },
+  );
+}
 
   // Login
-  Future<void> login({required String userName, required String password}) async {
+  Future<void> login({required String email, required String password}) async {
     state = state.copyWith(status: AuthStatus.loading);
-    final params = LoginUsecasesParams(userName: userName, password: password);
+    final params = LoginUsecasesParams(email: email, password: password);
     final result = await _loginUsecases.call(params);
     result.fold(
       (failure) {
